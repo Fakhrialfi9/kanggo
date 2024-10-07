38d7c2b43b93598d5e1f5f0a1d264cb59beb1915
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2024-10-08 05:02:53 +0700

    Prepare for deployment

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import App from "./base/apps/apps.vue";
 import router from "./base/routes/routes.ts";
-import "./base/style/index.js";
 import store from "./store/main.ts";
+import "./base/style/index.css";
 
 const app = createApp(App);
 app.use(store);
